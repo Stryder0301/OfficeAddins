ae0b128e8f8ca622f2e73c268c0ef04fb344cd29
Stryder0301 <63071905+Stryder0301@users.noreply.github.com>
2025-08-07 10:27:54 -0400

    Add files via upload

---
+++ Outlook/commands.js
@@ -0,0 +1,2 @@
+Office.onReady(function(){}),Office.actions.associate("action",function(e){Office.context.document.setSelectedDataAsync("This is a test"),e.completed()});
+//# sourceMappingURL=commands.js.map
